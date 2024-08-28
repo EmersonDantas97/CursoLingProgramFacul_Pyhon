518c5d045e886e2c98673215fdd9ba629254402d
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2024-08-28 20:06:50 -0300

    alguns comandos da primeira aula

---
+++ 01.HelloWord.py
@@ -0,0 +1,7 @@
+# Iniciando com Hello World, para trazer sorte.
+print('Hello World!') # Pode se utilizar aspas simples ou duplas.
+
+# Documentacao PEP 8.
+# Recomenda-se ter por linha, no maximo ate 79. 
+# Usar letras maiusculas separadas por underline para nome de classes. Ex: Minha_Classe.
+# Usar letras minusculas separadas por underline para nome de variaveis. Ex: minha_variavel.
---
+++ 02.VariaveisETipos.py
@@ -0,0 +1,10 @@
+x = 10 # Variavel do tipo inteiro.
+nome = 'aluno' # Variavel do tipo string.
+nota = 8.75 # Variavel do tipo float. Usa ponto inves de virgula. 
+fez_inscricao = True # Variavel do tipo bool.
+
+#print(type(X)) # Python e case sensitive, comando dara erro, por que declaramos a variavel em minusculo.
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(fez_inscricao))
